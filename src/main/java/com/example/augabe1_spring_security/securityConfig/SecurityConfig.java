e98776bb806ae15c7566f492e5d3609047f2a25e
Way-Maker7 <Denis_Sabou@gmx.de>
2022-06-27 15:40:01 +0200

    a2 punkt 3

--- src/main/java/com/example/augabe1_spring_security/securityConfig/SecurityConfig.java
+++ src/main/java/com/example/augabe1_spring_security/securityConfig/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.augabe1_spring_security.securityConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -11,10 +12,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("frank")
-                .password("knarf").roles("ADMIN");
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
     }
 
     @Bean
